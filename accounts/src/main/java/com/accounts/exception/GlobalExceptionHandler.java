95c13e3a68c4453b6bc75fa96026e0d2276d8ab4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-14 07:14:21 +0200

    Added exceptions

---
+++ accounts/src/main/java/com/accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.accounts.exception;
+
+import com.accounts.dto.ErrorResponseDto;
+import lombok.val;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(
+            CustomerAlreadyExistsException exception,
+            WebRequest webRequest
+    ) {
+        val errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
