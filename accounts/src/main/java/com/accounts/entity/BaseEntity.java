71ca528f781d84912249032ad3a6f6f605b1b39a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-17 09:02:39 +0200

    Added audit

--- accounts/src/main/java/com/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/accounts/entity/BaseEntity.java
@@ -1,26 +1,40 @@
 package com.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @ToString
 @MappedSuperclass
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     LocalDate createdAt;
+
+    @CreatedBy
     @Column(updatable = false)
     String createdBy;
+
+    @LastModifiedDate
     @Column(insertable = false)
     LocalDate updatedAt;
+
+    @LastModifiedBy
     @Column(insertable = false)
     String updatedBy;
 }
