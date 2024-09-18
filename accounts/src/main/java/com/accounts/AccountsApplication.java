a74ed0cf3843ec8aa46da7d85ad33a97bd093fff
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-18 17:45:45 +0200

    Added open-api

--- accounts/src/main/java/com/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/accounts/AccountsApplication.java
@@ -1,15 +1,40 @@
 package com.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Custom accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "SysoievB",
+                        email = "sysoiev@gmail.com",
+                        url = "http://localhost:8080/swagger-ui/index.html"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://localhost:8080/swagger-ui/index.html"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts microservice REST API Documentation",
+                url = "http://localhost:8080/swagger-ui/index.html"
+        )
+)
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
