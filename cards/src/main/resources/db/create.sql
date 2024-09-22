c139a8b4a758f87c978dc7276bab3704faa3ae7d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-22 15:07:22 +0200

    Added cards & loans microservices

--- cards/src/main/resources/db/create.sql
+++ cards/src/main/resources/db/create.sql
@@ -1,14 +1,15 @@
-CREATE TABLE IF NOT EXISTS `cards` (
-  `card_id` int NOT NULL AUTO_INCREMENT,
-  `mobile_number` varchar(15) NOT NULL,
-  `card_number` varchar(100) NOT NULL,
-  `card_type` varchar(100) NOT NULL,
-  `total_limit` int NOT NULL,
-  `amount_used` int NOT NULL,
-  `available_amount` int NOT NULL,
-  `created_at` date NOT NULL,
-  `created_by` varchar(20) NOT NULL,
-  `updated_at` date DEFAULT NULL,
-  `updated_by` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`card_id`)
+create table cards
+(
+    amount_used      integer not null,
+    available_amount integer not null,
+    total_limit      integer not null,
+    card_id          bigint  not null auto_increment,
+    created_at       datetime(6),
+    updated_at       datetime(6),
+    card_number      varchar(255),
+    card_type        varchar(255),
+    created_by       varchar(255),
+    mobile_number    varchar(255),
+    updated_by       varchar(255),
+    primary key (card_id)
 );
--- loans/src/main/resources/db/create.sql
+++ loans/src/main/resources/db/create.sql
@@ -1,14 +1,15 @@
-CREATE TABLE IF NOT EXISTS `loans` (
-  `loan_id` int NOT NULL AUTO_INCREMENT,
-  `mobile_number` varchar(15) NOT NULL,
-  `loan_number` varchar(100) NOT NULL,
-  `loan_type` varchar(100) NOT NULL,
-  `total_loan` int NOT NULL,
-  `amount_paid` int NOT NULL,
-  `outstanding_amount` int NOT NULL,
-  `created_at` date NOT NULL,
-  `created_by` varchar(20) NOT NULL,
-  `updated_at` date DEFAULT NULL,
-  `updated_by` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`loan_id`)
+create table loans
+(
+    amount_paid        integer not null,
+    outstanding_amount integer not null,
+    total_loan         integer not null,
+    created_at         datetime(6),
+    loan_id            bigint  not null auto_increment,
+    updated_at         datetime(6),
+    created_by         varchar(255),
+    loan_number        varchar(255),
+    loan_type          varchar(255),
+    mobile_number      varchar(255),
+    updated_by         varchar(255),
+    primary key (loan_id)
 );
